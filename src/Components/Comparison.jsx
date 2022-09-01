709baf873baffba5a7e2a8f4d771cb1c842f121c
Jared Shay <jared.shay@gmail.com>
2022-09-01 10:44:51 -0500

    Fix image_uris bug.
    The image_uris property previously returned an object of image types. It
    now returns a url.

--- src/Components/Comparison.jsx
+++ src/Components/Comparison.jsx
@@ -1,8 +1,7 @@
 export const Comparison = ({ cards, setCards, setComparisonCards, comparisonCards }) => {
-  
 
   const newComparison = (winner) => {
-    //adjusts the cards ELO, and sorts table accordingly 
+    //adjusts the cards ELO, and sorts table accordingly
       //note: cards with equivalent score are being rearranged. Possible to maintain score tiers?
     setCards(cards.map(obj => {
       if(obj.name === comparisonCards[winner].name){
@@ -10,8 +9,8 @@ export const Comparison = ({ cards, setCards, setComparisonCards, comparisonCard
       } else {
         return obj;
       }}).sort((a, b) => (a.score < b.score) ? 1 : -1))
-    
-    //sets the comparison cards to two random cards from the list  
+
+    //sets the comparison cards to two random cards from the list
     const randomCardNum = Math.floor(Math.random() * cards.length)
     const randomCardNum1 = Math.floor(Math.random() * cards.length)
     setComparisonCards([cards[randomCardNum], cards[randomCardNum1]])
@@ -24,10 +23,10 @@ export const Comparison = ({ cards, setCards, setComparisonCards, comparisonCard
       <div className="row">
         <div className="col-3"></div>
         <div className="col-3">
-          <img onClick={() => newComparison(0)} src={comparisonCards[0].image_uris?.normal} alt="" id="comparison-card"></img>
+          <img onClick={() => newComparison(0)} src={comparisonCards[0].image_uris} alt="" id="comparison-card"></img>
         </div>
         <div className="col-3">
-          <img onClick={() => newComparison(1)} src={comparisonCards[1].image_uris?.normal} alt="" id="comparison-card"></img>
+          <img onClick={() => newComparison(1)} src={comparisonCards[1].image_uris} alt="" id="comparison-card"></img>
         </div>
         <div className="col-3"></div>
       </div>
