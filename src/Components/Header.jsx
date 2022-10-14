b5f8b4d3ae5723ddc04d85ddced48f1b3706160e
dconnenc <dconnenc@gmail.com>
2022-10-14 10:55:19 -0500

    Improved Header Authentication Logic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,6 @@ import Profile from "./Auth0/profile";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
-
-
 export const Header = ({user}) => {
   const { isAuthenticated, isLoading } = useAuth0();
 
@@ -12,7 +10,7 @@ export const Header = ({user}) => {
     <div id="header" className=".container">
       <div className="row">
         <div className="col-3">
-          { isAuthenticated && !isLoading ? <Link to={`/profile/${user}`}><Profile /></Link> : <Profile />}
+          { isAuthenticated && !isLoading ? <Link to={`/profile/${user}`}><Profile /></Link> : <Profile /> }
         </div>
         <div className="col-6">
           <h1>My P1P1</h1>
