93f9ddf800ea12afbf326487773ca1bc583d0d63
Jared Shay <jared.shay@gmail.com>
2022-10-19 11:36:45 -0500

    inject port value

--- backend/node.js
+++ backend/node.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const cors = require('cors');
-const port = 3001
+const port = process.env.PORT || 3001
 
 
 app.use(cors({
   origin: '*'
-})); 
+}));
 
 app.use(bodyParser.json({limit: "50mb"}))
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
