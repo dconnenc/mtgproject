66e0df6c876dcc27d961caa658965b1b8bfbe8df
Jared Shay <jared.shay@gmail.com>
2022-10-19 11:45:32 -0500

    Manually add sslmode to db url

--- backend/queries.js
+++ backend/queries.js
@@ -6,7 +6,7 @@ const Pool = require('pg').Pool
 
 if (process.env.NODE_ENV === "production") {
   const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: `${process.env.DATABASE_URL}?sslmode=require`,
     ssl: {
       rejectUnauthorized: false
     }
