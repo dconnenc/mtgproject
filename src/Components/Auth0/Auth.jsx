d219ad5b0e2b9f9e04b8ff2619d4cd8540200b22
dconnenc <97249415+dconnenc@users.noreply.github.com>
2022-11-28 18:26:19 -0600

    Style updates // Font Face
    Added the font-face file to style fonts after Magic the Gatherings typeface.
    
    Improved CSS on the Auth Component

--- src/Components/Auth0/Auth.jsx
+++ src/Components/Auth0/Auth.jsx
@@ -4,8 +4,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export const Auth = () => {
 
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   var background = "https://preview.redd.it/qnnotlcehu731.jpg?auto=webp&s=55d9e57e829608fc8e632eb2e4165d816288177c"
+
+  const clickHandler = () => {
+    loginWithRedirect()
+  }
 
     if(isAuthenticated && !isLoading ) {
       window.location.href = "http://localhost:3000/"
@@ -16,7 +20,7 @@ export const Auth = () => {
           backgroundImage: `url(${background})`,
         }}>
           <Header />
-            <h1 className="greeting-text center">Please login!</h1>
+            <div className="greeting-text huge center-box" onClick={clickHandler}>Please login</div>
           <Footer />
         </div>
       )
